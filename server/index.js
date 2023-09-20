f187e4a980363c3ddb4e8ddef2cd5a0367bc449e
Abhishek-ai-ml <abhishekdoulia@gmail.com>
2023-09-20 16:55:03 +0530

    try

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://studytech-frontend.vercel.app/",
+        origin:"https://studytech-frontend.vercel.app",
         credentials:true,
     })
 );
